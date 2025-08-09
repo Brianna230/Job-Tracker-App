76bebc12da3fb4fd3949a3e461342e7880213278
Brianna Lamarre <briannalamarre@Briannas-Laptop.local>
2025-08-08 21:06:31 -0400

    I am working on the frontend

--- front-end/src/Components/FetchTest.jsx
+++ front-end/src/Components/FetchTest.jsx
@@ -6,17 +6,19 @@ const[jobs, setJobs] = useState([])
         const fetchData = async ()=>{
             const response = await fetch('http://127.0.0.1:5000/api/jobs')
             const jsonData = await response.json();
-            setJobs(jsonData.Jobs)
+            setJobs(jsonData)
         };
         fetchData();
     }, []);
+
+   
 
     return(
         <div>
             <h1>Job List</h1>
             <ul>
                 {jobs.map((job,index)=>(
-                    <li key={index}>{job}</li>
+                    <li key={index}>{job.title}<br></br>{job.apply_link}<br></br>{job.company}<br></br>{job.location}</li>
                 ))}
             </ul>
         </div>
